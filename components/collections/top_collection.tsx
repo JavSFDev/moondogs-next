e65a66548df84897e12b635c1b5f2ec8fee0a799
JavSFDev <javsfdev323@gmail.com>
2023-04-22 22:48:53 +0300

    fix e.map is not function bug.

--- components/collections/top_collection.tsx
+++ components/collections/top_collection.tsx
@@ -40,6 +40,66 @@ const Top_collection = () => {
             </div>
           </div>
 
+          <div className="grid grid-cols-1 gap-3 md:grid-cols-2 md:gap-[1.875rem] lg:grid-cols-4">
+            {topCollections && topCollections !== undefined
+              ? topCollections?.[0]?.map((collection, i) => {
+                  const { id, bannerImage, name, verified, volume } =
+                    collection;
+
+                  return (
+                    <div
+                      className="flex py-4 transition-shadow bg-white border border-jacarta-100 dark:bg-jacarta-700 rounded-2xl px-7 hover:shadow-lg dark:border-transparent"
+                      key={id}
+                    >
+                      <figure className="mr-4 shrink-0">
+                        <a
+                          className="relative block"
+                          href={"/collection/" + id}
+                        >
+                          <img
+                            src={bannerImage || "/images/team/team_1.jpg"}
+                            alt={name}
+                            height={48}
+                            width={48}
+                            className="rounded-2lg"
+                          />
+                          <div className="absolute flex items-center justify-center w-6 h-6 text-xs text-white border-2 border-white rounded-full dark:border-jacarta-600 bg-jacarta-700 -left-3 top-1/2 -translate-y-2/4">
+                            {i + 1}
+                          </div>
+                          {verified && (
+                            <div
+                              className="dark:border-jacarta-600 bg-green absolute -left-3 top-[60%] flex h-6 w-6 items-center justify-center rounded-full border-2 border-white"
+                              data-tippy-content="Verified Collection"
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                width="24"
+                                height="24"
+                                className="h-[.875rem] w-[.875rem] fill-white"
+                              >
+                                <path fill="none" d="M0 0h24v24H0z"></path>
+                                <path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
+                              </svg>
+                            </div>
+                          )}
+                        </a>
+                      </figure>
+                      <div>
+                        <a className="block" href={"/collection/" + id}>
+                          <span className="font-semibold font-display text-jacarta-700 hover:text-accent dark:text-white">
+                            {name?.substring(0, 10)}
+                          </span>
+                        </a>
+                        <span className="text-sm dark:text-jacarta-300">
+                          {volume} CORE
+                        </span>
+                      </div>
+                    </div>
+                  );
+                })
+              : null}
+          </div>
           <div className="mt-10 text-center">
             <Link href="/rankings">
               <a className="inline-block px-8 py-3 font-semibold text-center text-white transition-all rounded-full bg-accent shadow-accent-volume hover:bg-accent-dark">
